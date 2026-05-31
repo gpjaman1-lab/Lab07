8070cb9787bb7a4375093b1ec5b3311c41003b3d
Tanvir <gpjaman1@gmail.com>
2026-05-31 17:07:42 +0800

    Added search form and search result

---
+++ search_form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Car Search</title>
+</head>
+<body>
+
+<h2>Search Car Model</h2>
+
+<form action="search_result.php" method="GET">
+    <input type="text" name="model" placeholder="Enter car model">
+    <input type="submit" value="Search">
+</form>
+
+</body>
+</html>
